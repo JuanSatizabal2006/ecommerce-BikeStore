9a7cd0b24696ef4d89118b94d733429a3edda2f2
JuanSatizabal <129564629+JuanSatizabal2006@users.noreply.github.com>
2025-01-31 15:43:19 -0500

    jijija!

--- backEnd/src/controllers/articulos.controller.js
+++ backEnd/src/controllers/articulos.controller.js
@@ -1,83 +1,115 @@
 const db = require("../db");
 const { jsonRespuesta } = require("../lib/jsonRespuesta");
 
+//------------------PRUEBAS
+
+const pruebas = async (req, res) => {
+  try {
+    const query = await db.query("SELECT * FROM departamentos");
+    res.json(query.rows);
+  } catch (error) {
+    res.json(error.message);
+  }
+};
+
+//-------------------------
+
 const getArticulos = async (req, res, next) => {
-
-    try {
-        const allArticulos = await db.query("SELECT * FROM articulos WHERE stock >= 1 ORDER BY descuento DESC, id_articulo ASC");
-        res.json(allArticulos.rows);
-        //res.send('Hola soy la funcion externa y estoy obteniendo todos los articulos');
-    } catch (error) {
-        next(error)
-    }
-
+  try {
+    const allArticulos = await db.query(
+      "SELECT * FROM articulos WHERE stock >= 1 ORDER BY 'idArticulo' ASC"
+    );
+    res.json(allArticulos.rows);
+    //res.send('Hola soy la funcion externa y estoy obteniendo todos los articulos');
+  } catch (error) {
+    next(error);
+  }
 };
 
 const getOneArticulo = async (req, res) => {
-
-    try {
-        const { id } = req.params;
-        //*Convertimos el id a un dato entero
-        const idProducto = parseInt(id);
-        //*Validamos que el id que nos pasan no sea un texto, null, undefined
-        if (!idProducto || typeof idProducto == "string") {
-            return res.status(404).json(jsonRespuesta(404, {error: '¡Articulo no encontrado!'}));
-        }
-
-        const oneArticulo = await db.query('SELECT * FROM articulos WHERE id_articulo = $1', [ idProducto ]);
-
-        //*No se encuentra un articulo
-        if(oneArticulo.rows.length === 0){
-            return res.status(404).json(jsonRespuesta(404, {error: '¡Articulo no encontrado!'}))
-        }
-
-        res.status(200).json(jsonRespuesta(200 , oneArticulo.rows));
-
-    } catch (error) {
-        console.log(error.message);
+  try {
+    const { id } = req.params;
+    //*Convertimos el id a un dato entero
+    const idProducto = parseInt(id);
+    //*Validamos que el id que nos pasan no sea un texto, null, undefined
+    if (!idProducto || typeof idProducto == "string") {
+      return res
+        .status(404)
+        .json(jsonRespuesta(404, { error: "¡Articulo no encontrado!" }));
     }
-    
+
+    const oneArticulo = await db.query(
+      "SELECT * FROM articulos WHERE id_articulo = $1",
+      [idProducto]
+    );
+
+    //*No se encuentra un articulo
+    if (oneArticulo.rows.length === 0) {
+      return res
+        .status(404)
+        .json(jsonRespuesta(404, { error: "¡Articulo no encontrado!" }));
+    }
+
+    res.status(200).json(jsonRespuesta(200, oneArticulo.rows));
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
-const getArticulosHome = async (req,res) =>{
-    const response = await db.query("SELECT * FROM public.articulos WHERE stock >= 1 ORDER BY descuento DESC, id_articulo ASC LIMIT 3");
-    console.log(response.rows);
-    res.status(200).json(jsonRespuesta(200, response.rows));
-}
+const getArticulosHome = async (req, res) => {
+  const response = await db.query(
+    "SELECT * FROM public.articulos WHERE stock >= 1 ORDER BY descuento DESC, id_articulo ASC LIMIT 3"
+  );
+  console.log(response.rows);
+  res.status(200).json(jsonRespuesta(200, response.rows));
+};
 
 const putArticulos = async (req, res, next) => {
-    try {
-        const { id } = req.params;
-        const { nombre , costo, margen, descuento, id_categoria } = req.body;
-        
-        //*const selectPreview = db.query("SELECT * FROM articulos WHERE id_articulo = $1", [])
+  try {
+    const { id } = req.params;
+    const { nombre, costo, margen, descuento, id_categoria } = req.body;
 
-        const resultPut = await db.query("UPDATE articulos SET nombre = $1 , costo = $2, margen = $3, descuento = $4, id_categoria = $5 WHERE id_articulo = $6",[ nombre , costo, margen, descuento, id_categoria, id ])
-    
-        if(resultPut.rows.length === 0){
-            return res.status(404).json({message : "Articulo no encontrado"})
-        }
-    
-        res.json(resultPut);
-    } catch (error) {
-        next(error)
+    //*const selectPreview = db.query("SELECT * FROM articulos WHERE id_articulo = $1", [])
+
+    const resultPut = await db.query(
+      "UPDATE articulos SET nombre = $1 , costo = $2, margen = $3, descuento = $4, id_categoria = $5 WHERE id_articulo = $6",
+      [nombre, costo, margen, descuento, id_categoria, id]
+    );
+
+    if (resultPut.rows.length === 0) {
+      return res.status(404).json({ message: "Articulo no encontrado" });
     }
-}
 
-const deleteArticulos = async (req, res, next) => {
-    try {
-        const { id } = req.params;
-
-        const result = await db.query('DELETE FROM articulos WHERE id_articulo = $1', [ id ]);
-    
-        if(result.rowCount === 0){
-            return res.status(404).send("La tarea no se encuentra")
-        }
-        res.json(result);
-        //res.sendStatus(204);
-    } catch (error) {
-        next(error)
-    }
+    res.json(resultPut);
+  } catch (error) {
+    next(error);
+  }
 };
 
-module.exports ={ getArticulos, getOneArticulo, putArticulos, deleteArticulos, getArticulosHome }
+const deleteArticulos = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const result = await db.query(
+      "DELETE FROM articulos WHERE id_articulo = $1",
+      [id]
+    );
+
+    if (result.rowCount === 0) {
+      return res.status(404).send("La tarea no se encuentra");
+    }
+    res.json(result);
+    //res.sendStatus(204);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  getArticulos,
+  getOneArticulo,
+  putArticulos,
+  deleteArticulos,
+  getArticulosHome,
+  pruebas
+};
--- backEnd/src/routes/bike.routes.js
+++ backEnd/src/routes/bike.routes.js
@@ -1,38 +1,67 @@
 const { Router } = require("express");
-const { getUsuario, postUsuario, getUserToken, getOneUsuario } = require("../controllers/usuarios.controller");
-const { getArticulos, getOneArticulo, putArticulos, deleteArticulos, getArticulosHome } = require("../controllers/articulos.controller");
+const {
+  getUsuario,
+  postUsuario,
+  getUserToken,
+  getOneUsuario,
+} = require("../controllers/usuarios.controller");
+const {
+  getArticulos,
+  getOneArticulo,
+  putArticulos,
+  deleteArticulos,
+  getArticulosHome,
+  pruebas,
+} = require("../controllers/articulos.controller");
 const { autenticarToken } = require("../middlewares/autenticarToken");
 const { autenticarValores } = require("../middlewares/autenticarValores");
 const { doCompraUser } = require("../controllers/compras.controller");
 const { getOnePedido } = require("../controllers/pedidos.controller");
 const { getAllVentas } = require("../controllers/admin/dashboard");
-const { cantStockDisponible, cantStockNoDisponible, cantVentas, cantUsuarios, masVendidos, menosVendidos, ultimasVentas } = require("../controllers/admin/recuentosDashboard");
-const { allProductosInventario, oneProducto } = require("../controllers/admin/inventario");
+const {
+  cantStockDisponible,
+  cantStockNoDisponible,
+  cantVentas,
+  cantUsuarios,
+  masVendidos,
+  menosVendidos,
+  ultimasVentas,
+} = require("../controllers/admin/recuentosDashboard");
+const {
+  allProductosInventario,
+  oneProducto,
+} = require("../controllers/admin/inventario");
 const { allPedidos, onePedido } = require("../controllers/admin/pedidos");
-const { postArticulos, getUltimoId, getUltimaReferencia } = require("../controllers/admin/articulos.controller");
+const {
+  postArticulos,
+  getUltimoId,
+  getUltimaReferencia,
+} = require("../controllers/admin/articulos.controller");
 const { subirImagen } = require("../controllers/admin/subirImagen");
 
-const multer = require('multer');
-const upload = multer({dest: 'uploads/'})
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
 const router = Router();
 
+
+router.get("/prueba", pruebas);
 //TODO: Sobre los articulos
 router.get("/articulos", getArticulos);
 router.get("/articulos/:id", getOneArticulo);
-router.get("/home/articulos", getArticulosHome)
+router.get("/home/articulos", getArticulosHome);
 
 router.delete("/articulos/:id", deleteArticulos);
 
 router.put("/articulos/:id", putArticulos);
 
-//TODO:Sobre los usuarios  
+//TODO:Sobre los usuarios
 //*login
 router.post("/login", getUsuario);
 //*verificacion del token
 router.get("/usuario", autenticarToken, getUserToken);
 //*registro del usuario
-router.post("/registro", autenticarValores ,postUsuario);
+router.post("/registro", autenticarValores, postUsuario);
 
 router.get("/obtenerusuario/:id", getOneUsuario);
 
@@ -70,12 +99,17 @@ router.get("/onearticulo/:idArticulo", oneProducto);
 //*Pedidos
 router.get("/pedidosadmin", allPedidos);
 
-router.get('/pedidosadmin/one/:idVenta/:idArticulo', onePedido);
+router.get("/pedidosadmin/one/:idVenta/:idArticulo", onePedido);
 
 //*Subir Productos
-router.post("/subirimagen", upload.array('fotos', 5) , subirImagen, postArticulos);
+router.post(
+  "/subirimagen",
+  upload.array("fotos", 5),
+  subirImagen,
+  postArticulos
+);
 
-router.get('/id', getUltimoId);
-router.get('/referencia/:id', getUltimaReferencia);
+router.get("/id", getUltimoId);
+router.get("/referencia/:id", getUltimaReferencia);
 
 module.exports = router;
